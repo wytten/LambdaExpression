07f9c748ce8487d5635aba25865821d727b67553
Dale Wyttenbach <dwyttenbach@decare.com>
2023-11-21 13:27:09 -0600

    cleanup

--- src/com/modernjava/funcprogramming/VariableScope.java
+++ src/com/modernjava/funcprogramming/VariableScope.java
@@ -4,14 +4,16 @@ import java.util.List;
 import java.util.function.IntConsumer;
 
 public class VariableScope {
+    // class variable
     static int k=0;
+    
     public static void main(String[] args) {
         int b =10; //local variable
         IntConsumer intConsumer = (a) -> System.out.println(a*10);
 
         List<Instructor> instructors = Instructors.getAll();
         instructors.forEach(instructor -> {
-            System.out.println(instructor + " " +  k);
+            System.out.println(k++ +" " +instructor);
         });
         k++;
 
